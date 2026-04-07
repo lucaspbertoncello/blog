d60a317a04efd3e452091d3c9c0a33941106f2ef
lucaspbertoncello <lucasbertoncello1@gmail.com>
2026-04-06 21:56:21 -0300

    feat: post confirmation create user entity on dynamo

---
+++ blog-api/src/functions/auth/postConfirmation.ts
@@ -0,0 +1,31 @@
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { PostConfirmationTriggerEvent } from "aws-lambda";
+import { dynamoClient } from "../../clients/dynamoClient";
+
+export const handler = async (event: PostConfirmationTriggerEvent) => {
+  const { email, sub } = event.request.userAttributes;
+
+  const now = new Date().toISOString();
+
+  const command = new PutCommand({
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      PK: `ACCOUNT#${sub}`,
+      SK: "INFO",
+      GSI1PK: `EMAIL#${email}`,
+      GSI1SK: `ACCOUNT#${sub}`,
+      GSI2SK: `CREATED_AT#${now}`,
+      accountId: sub,
+      email,
+      role: "student",
+      active: true,
+      createdAt: now,
+      updatedAt: now,
+    },
+  });
+
+  const response = await dynamoClient.send(command);
+  console.log(response);
+
+  return event;
+};
