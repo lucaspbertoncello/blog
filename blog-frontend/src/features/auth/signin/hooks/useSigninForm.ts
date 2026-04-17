60adc3ae406f325ba4c76f6dda901d981ec25c8a
lucaspbertoncello <lucasbertoncello1@gmail.com>
2026-04-17 00:58:01 -0300

    feat(auth): redirect and save tokens on signin success

--- blog-frontend/src/features/auth/signin/hooks/useSigninForm.ts
+++ blog-frontend/src/features/auth/signin/hooks/useSigninForm.ts
@@ -1,6 +1,9 @@
 import { useForm } from "@tanstack/react-form";
 import { z } from "zod";
 import { useSignin } from "@/domain/auth/hooks/useSignin";
+import { useNavigate } from "@tanstack/react-router";
+import { useAuthStore } from "@/domain/auth/stores/useAuthStore";
+import { useShallow } from "zustand/react/shallow";
 
 export const signinSchema = z.object({
   email: z.string().min(1, "Email obrigatório").email("Endereço de email inválido"),
@@ -9,6 +12,12 @@ export const signinSchema = z.object({
 
 export function useSigninForm() {
   const { mutate, isPending } = useSignin();
+  const navigate = useNavigate({ from: "/signin" });
+  const { setAuthTokens } = useAuthStore(
+    useShallow((state) => ({
+      setAuthTokens: state.setAuthTokens,
+    }))
+  );
 
   const form = useForm({
     defaultValues: {
@@ -19,7 +28,12 @@ export function useSigninForm() {
       onChange: signinSchema,
     },
     onSubmit: ({ value }) => {
-      mutate(value);
+      mutate(value, {
+        onSuccess: (response) => {
+          navigate({ to: "/" });
+          setAuthTokens(response);
+        },
+      });
     },
   });
 
