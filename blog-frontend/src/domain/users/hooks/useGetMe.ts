c713019d8a0f5955717e734c16f8b1bfed8ef9f8
lucaspbertoncello <lucasbertoncello1@gmail.com>
2026-04-17 03:40:41 -0300

    fix(auth): delete localStorage token bypasses get me validation

--- blog-frontend/src/domain/users/hooks/useGetMe.ts
+++ blog-frontend/src/domain/users/hooks/useGetMe.ts
@@ -1,14 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { getMe } from "../services/getMe";
-import { useAuthStore } from "@/domain/auth/stores/useAuthStore";
 
 export function useGetMe() {
-  const getIsAuthenticated = useAuthStore((state) => state.getIsAuthenticated);
   const methods = useQuery({
     queryFn: getMe,
     queryKey: ["getMe"],
     retry: false,
-    enabled: getIsAuthenticated(),
   });
 
   return methods;
--- blog-frontend/src/shared/guards/PublicRouteLayout.tsx
+++ blog-frontend/src/shared/guards/PublicRouteLayout.tsx
@@ -3,7 +3,6 @@ import { useGetMe } from "@/domain/users/hooks/useGetMe";
 import { useUserStore } from "@/domain/users/stores/useUserStore";
 import { Outlet } from "@tanstack/react-router";
 import { useEffect } from "react";
-import { toast } from "sonner";
 
 export function PublicRouteLayout() {
   const { data, isError } = useGetMe();
@@ -14,7 +13,6 @@ export function PublicRouteLayout() {
   useEffect(() => {
     if (isError) {
       clearAuthTokens();
-      toast.error("Faça login novamente");
       return;
     }
 
