597ee9633b858c68d7a0ef3f4d15aa114b106b00
lucaspbertoncello <lucasbertoncello1@gmail.com>
2026-04-23 16:24:42 -0300

    refactor(adapter): move internal types into LambdaHttpAdapter namespace

--- blog-api/src/adapters/lambdaHttpAdapter.ts
+++ blog-api/src/adapters/lambdaHttpAdapter.ts
@@ -10,33 +10,17 @@ import { HandlerParams, HandlerResponse } from "../types/Handlers";
 import { ApplicationError } from "../errors/ApplicationError";
 import { Role } from "../types/Roles";
 
-type Route = "public" | "private";
-type EventType<TRoute extends Route> = TRoute extends "public"
-  ? APIGatewayProxyEventV2
-  : APIGatewayProxyEventV2WithJWTAuthorizer;
-
-type AdapterFn<
-  TBody extends Record<string, any> | undefined,
-  TParams extends Record<string, any> | undefined,
-  TQueryParams extends Record<string, any> | undefined,
-  TResponse,
-> = (params: HandlerParams<TBody, TParams, TQueryParams>) => Promise<HandlerResponse<TResponse>>;
-
-type AdapterOptions = {
-  schema?: ZodType;
-  querySchema?: ZodType;
-  errorMapper?: (err: unknown) => ApplicationError;
-  requiredRoles?: Role[];
-};
-
 export function lambdaHttpAdapter<
-  TRoute extends Route,
+  TRoute extends LambdaHttpAdapter.Route,
   TBody extends Record<string, any> | undefined = undefined,
   TResponse = undefined,
   TParams extends Record<string, any> | undefined = undefined,
   TQueryParams extends Record<string, any> | undefined = undefined,
->(fn: AdapterFn<TBody, TParams, TQueryParams, TResponse>, options?: AdapterOptions) {
-  return async (event: EventType<TRoute>): Promise<APIGatewayProxyResultV2> => {
+>(
+  fn: LambdaHttpAdapter.AdapterFn<TBody, TParams, TQueryParams, TResponse>,
+  options?: LambdaHttpAdapter.AdapterOptions,
+) {
+  return async (event: LambdaHttpAdapter.EventType<TRoute>): Promise<APIGatewayProxyResultV2> => {
     try {
       const rawBody = bodyParser(event.body);
       const body: TBody = options?.schema ? options.schema.parse(rawBody) : rawBody;
@@ -93,5 +77,26 @@ export function lambdaHttpAdapter<
       console.error(err);
       return sendResponse({ statusCode: 500, body: { message: "Erro interno do servidor" } });
     }
+  };
+}
+
+export namespace LambdaHttpAdapter {
+  export type Route = "public" | "private";
+  export type EventType<TRoute extends Route> = TRoute extends "public"
+    ? APIGatewayProxyEventV2
+    : APIGatewayProxyEventV2WithJWTAuthorizer;
+
+  export type AdapterFn<
+    TBody extends Record<string, any> | undefined,
+    TParams extends Record<string, any> | undefined,
+    TQueryParams extends Record<string, any> | undefined,
+    TResponse,
+  > = (params: HandlerParams<TBody, TParams, TQueryParams>) => Promise<HandlerResponse<TResponse>>;
+
+  export type AdapterOptions = {
+    schema?: ZodType;
+    querySchema?: ZodType;
+    errorMapper?: (err: unknown) => ApplicationError;
+    requiredRoles?: Role[];
   };
 }
